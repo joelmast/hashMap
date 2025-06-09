63d08cc7f15d9ec945c2c24e21ad5f18d4c40d3a
Hamburger-coder <joesmast@gmail.com>
2025-06-09 17:46:41 -0400

    implement hash() and start on set() methods

--- script.js
+++ script.js
@@ -2,19 +2,40 @@ class HashMap {
     constructor() {
         this.loadFactor = 0.75;
         this.capacity = 16;
+        this.amount = 0;
+        this.arr = new Array(this.capacity);
     }
     hash(key) {
-        function hash(key) {
-            let hashCode = 0;
-            const primeNumber = 31;
-            for (let i = 0; i < key.length; i++) {
-                hashCode = primeNumber * hashCode + key.charCodeAt(i);
-            }
+        let hashCode = 0;
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = hashCode % this.capacity;
+        }
 
-            return hashCode;
-        } 
+        return hashCode;
     }
     set(key, value) {
+        let myHash = this.hash(key);
+        if (!this.arr[myHash] || this.arr[myHash].length === 0) {
+            this.arr[myHash] = [{key, value}]
+            this.amount++;
+            if (this.amount / this.capacity > this.loadFactor) {
+                this.capacity = this.capacity * 2;
+                oldArr = this.arr.splice();
+                // iterate over array and add new ones
+                 
+            }
+        } else {
+            for(obj of this.arr[myHash]) {
+                if (obj.key === key) {
+                    obj.value = value;
+                    return;
+                }
+            }
+            this.arr[myHash].push({key, value});
+            this.amount++;
+        }
 
     }
     get(key) {
